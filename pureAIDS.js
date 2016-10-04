0d8e6c807ca28f20032f71d7bbd262f93ff7943b
Bram ten Lohuis <bramtenlohuis@gmail.com>
2016-10-04 14:33:31 +0200

    thurdTimeIsACharm

--- pureAIDS.js
+++ pureAIDS.js
@@ -3,3 +3,7 @@ Welkom op de AIDS party!
 -
 Maak er wat moois van, 1 miljoen lines of cancer code, here we cum!
 */
+function getRandomNumber(n) {
+  //This is an advancent random generator that always gives you a different number
+  return 3;
+}
